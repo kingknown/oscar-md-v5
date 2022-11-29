939b3446fde09e46034f9aed4694435dc8073a52
kingknown <118806215+kingknown@users.noreply.github.com>
2022-11-29 12:06:34 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 「 Suport Bot Dengan Cara donasi 」
 
-⫹⫺ Via Pulsa Indosat : 6285850539404
-⫹⫺ Via Dana : 6285850539404
+⫹⫺ Via Pulsa Indosat : chatowner
+⫹⫺ Via Dana : chatowner
 ⫹⫺ Via Saweria :
-⫹⫺ https://saweria.co/jarotoffc
+⫹⫺ chatowner
 
 `.trim()
   m.reply(text)
